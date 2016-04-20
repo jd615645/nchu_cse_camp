9c1cc16e46413708acc455c24555a67533e86f0e
jd615645 <jd615645@gmail.com>
2016-04-21 01:11:05 +0800

    ":cat2:新增簡介及報名資訊頁面"

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 jQuery(document).ready(function($) {
-	$('#main').fullpage({
-		sectionsColor: ['#0A0A0A', '#393E46', '#303841', '#3A4750'],
-		'slidesNavigation': false,
-	});
+  $('#main').fullpage({
+    sectionsColor: ['#0A0A0A', '#393E46', '#303841', '#3A4750'],
+    'slidesNavigation': false,
+  });
+
+  $(window).resize(function(){
+      autoScrolling();
+  });
+
+  function autoScrolling(){
+    var $window_width = $(window).width();
+    if($window_width < 767){
+        $.fn.fullpage.setAutoScrolling(false);
+    } else {
+        $.fn.fullpage.setAutoScrolling(true);
+    }
+  }
+
+  autoScrolling();
 });
 // '#222831'
