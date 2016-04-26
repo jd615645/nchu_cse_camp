b134e212f753af74ec4f018ef46f9a79c7a8fe29
Sakamoto <sakamoto@Thinkpad>
2016-04-26 13:07:52 +0800

    :cat2:change title background-color

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
   $('#main').fullpage({
-    sectionsColor: ['#0A0A0A', '#393E46', '#303841', '#3A4750', '#222831']
+    sectionsColor: ['#030303', '#393E46', '#303841', '#3A4750', '#222831']
   });
 
   $(window).resize(function(){
