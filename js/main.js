9256daf8c45268c6461d2524567175ea5551651c
jd615645 <jd615645@gmail.com>
2016-05-01 20:17:47 +0800

    :cat2:更改課程表與主辦協辦單位資訊

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
   $('#main').fullpage({
-    sectionsColor: ['#030303', '#393E46', '#303841', '#3A4750', '#222831']
+    sectionsColor: ['#0A0A0A', '#393E46', '#303841', '#3A4750', '#222831']
   });
 
   $(window).resize(function(){
@@ -15,6 +15,19 @@ jQuery(document).ready(function($) {
         $.fn.fullpage.setAutoScrolling(true);
     }
   }
+
+  $('.logo_img').dimmer({
+    on: 'hover'
+  });
+
+  $('.schedule_detail').click(function() {
+    var which_detail = $(this).attr('value');
+    $('#schedule_detai_' + which_detail).modal('show');
+  });
+  $('.ui.modal .close').click(function() {
+    var which_detail = $(this).attr('value');
+    $('#schedule_detai_' + which_detail).modal('hide');
+  });
 
   autoScrolling();
 });
